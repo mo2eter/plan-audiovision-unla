0a91ebebcda22a0f50d3f0df00f6d124ec63688b
mo2eter <micamosqueira@gmail.com>
2025-07-16 21:48:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ const materias = [
   { nombre: "Registro de Sonido en Audiovisuales", correlativas: [] },
   { nombre: "Montaje 1", correlativas: [] },
   { nombre: "Fotografía e Iluminación en Audiovisuales", correlativas: [] },
-  { nombre: "Inglés I", correlativas: [] },
-
+  { nombre: "Inglés I", correlativas: [] }
+],
   "SEGUNDO AÑO": [
   { nombre: "Lenguaje Audiovisual 2", correlativas: ["Lenguaje Audiovisual 1", "Fotografía e Iluminación en Audiovisuales"] },
   { nombre: "Audición y Análisis Musical", correlativas: ["Taller de Lectura y Escritura profesional", "Educación Auditiva"] },
@@ -19,26 +19,26 @@ const materias = [
   { nombre: "Realización Integral Audiovisual 1", correlativas: ["Lenguaje Audiovisual 2", "Taller de Edición de Sonido", "Taller de Montaje"] },
   { nombre: "Arte y Sociedad", correlativas: ["Audición y Análisis Musical"] },
   { nombre: "Guión", correlativas: ["Lenguaje Audiovisual 2"] },
-  { nombre: "Taller de Composición Sonora", correlativas: ["Audición y Análisis Musical", "Taller de Edición de Sonido"] },
-
+  { nombre: "Taller de Composición Sonora", correlativas: ["Audición y Análisis Musical", "Taller de Edición de Sonido"] }
+],
   "TERCER AÑO": [
   { nombre: "Banda Sonora", correlativas: ["Taller de Composición Sonora"] },
   { nombre: "Géneros Estilos Audiovisuales", correlativas: ["Arte y Sociedad"] },
   { nombre: "Taller de Experimentación Audiovisual", correlativas: ["Lenguaje Audiovisual 2", "Taller de Composición Sonora"] },
   { nombre: "Realización Integral Audiovisual 2", correlativas: ["Realización Integral Audiovisual 1", "Guión"] },
-  { nombre: "Ética Profesional", correlativas: ["Taller de Lectura y Escritura profesional"] },
-
+  { nombre: "Ética Profesional", correlativas: ["Taller de Lectura y Escritura profesional"] }
+],
   "SEMINARIOS": [
   { nombre: "Seminario de Pensamiento Nacional y Latinoamericano", correlativas: [] },
-  { nombre: "Seminario de Justicia y Derechos Humanos", correlativas: [] },
-
+  { nombre: "Seminario de Justicia y Derechos Humanos", correlativas: [] }
+],
   "CICLO DE FORMACIÓN ORIENTADA": [
   { nombre: "Gestión de proyectos Audiovisuales", correlativas: ["Realización Integral Audiovisual 2"] },
   { nombre: "Estética", correlativas: ["Arte y Sociedad"] },
   { nombre: "Comunicación Audiovisual", correlativas: ["Taller de Lectura y Escritura profesional"] },
   { nombre: "Tecnología de Postproducción 1", correlativas: ["Tecnología de la Imagen", "Electrónica de las Comunicaciones"] },
-  { nombre: "Imagen, Tipografía e Identidad Cultural", correlativas: ["Taller de Lectura y Escritura profesional"] },
-
+  { nombre: "Imagen, Tipografía e Identidad Cultural", correlativas: ["Taller de Lectura y Escritura profesional"] }
+],
   "CUARTO AÑO": [
   { nombre: "Semiótica", correlativas: ["Taller de Lectura y Escritura profesional"] },
   { nombre: "Inglés II", correlativas: ["Inglés I"] },
@@ -49,8 +49,8 @@ const materias = [
   { nombre: "Animación 3D 1", correlativas: ["Edición y Corrección Digital de Imagen"] },
   { nombre: "Composición Digital", correlativas: ["Edición y Corrección Digital de Imagen"] },
   { nombre: "Tecnología de Postproducción 3", correlativas: ["Tecnología de Postproducción 2"] },
-  { nombre: "Optativa 1", correlativas: [] },
-
+  { nombre: "Optativa 1", correlativas: [] }
+],
   "QUINTO AÑO": [
   { nombre: "Taller de Trabajo Final Integrador Mención Postproducción de Imagen", correlativas: ["Géneros Estilos Audiovisuales", "Gestión de proyectos Audiovisuales", "Estética", "Imagen, Tipografía e Identidad Cultural", "Semiótica"] },
   { nombre: "Animación 3D 2", correlativas: ["Animación 3D 1", "Composición Digital"] },
@@ -58,7 +58,7 @@ const materias = [
   { nombre: "Práctica Preprofesional Mención Postproducción de Imagen", correlativas: ["Realización Integral Audiovisual 2"] },
   { nombre: "Optativa 2", correlativas: [] },
   { nombre: "Optativa 3", correlativas: [] }
-};
+];
 
 let aprobadas = new Set();
 
