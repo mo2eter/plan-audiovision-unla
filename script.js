0e8be13f934d4ac01e5033e464e675690f9569e9
mo2eter <micamosqueira@gmail.com>
2025-07-16 22:02:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const materiasPorAnio = {
-  "PRIMER AÑO": [
+"PRIMER AÑO": [
   { nombre: "Tecnología de la Imagen", correlativas: [] },
   { nombre: "Elementos de Audio", correlativas: [] },
   { nombre: "Taller de Lectura y Escritura profesional", correlativas: [] },
@@ -11,7 +11,7 @@ const materiasPorAnio = {
   { nombre: "Fotografía e Iluminación en Audiovisuales", correlativas: [] },
   { nombre: "Inglés I", correlativas: [] }
 ],
-  "SEGUNDO AÑO": [
+"SEGUNDO AÑO": [
   { nombre: "Lenguaje Audiovisual 2", correlativas: ["Lenguaje Audiovisual 1", "Fotografía e Iluminación en Audiovisuales"] },
   { nombre: "Audición y Análisis Musical", correlativas: ["Taller de Lectura y Escritura profesional", "Educación Auditiva"] },
   { nombre: "Taller de Edición de Sonido", correlativas: ["Elementos de Audio", "Registro de Sonido en Audiovisuales"] },
@@ -21,25 +21,25 @@ const materiasPorAnio = {
   { nombre: "Guión", correlativas: ["Lenguaje Audiovisual 2"] },
   { nombre: "Taller de Composición Sonora", correlativas: ["Audición y Análisis Musical", "Taller de Edición de Sonido"] }
 ],
-  "TERCER AÑO": [
+"TERCER AÑO": [
   { nombre: "Banda Sonora", correlativas: ["Taller de Composición Sonora"] },
   { nombre: "Géneros Estilos Audiovisuales", correlativas: ["Arte y Sociedad"] },
   { nombre: "Taller de Experimentación Audiovisual", correlativas: ["Lenguaje Audiovisual 2", "Taller de Composición Sonora"] },
   { nombre: "Realización Integral Audiovisual 2", correlativas: ["Realización Integral Audiovisual 1", "Guión"] },
   { nombre: "Ética Profesional", correlativas: ["Taller de Lectura y Escritura profesional"] }
 ],
-  "SEMINARIOS": [
+"SEMINARIOS": [
   { nombre: "Seminario de Pensamiento Nacional y Latinoamericano", correlativas: [] },
   { nombre: "Seminario de Justicia y Derechos Humanos", correlativas: [] }
 ],
-  "CICLO DE FORMACIÓN ORIENTADA": [
+"CICLO DE FORMACIÓN ORIENTADA": [
   { nombre: "Gestión de proyectos Audiovisuales", correlativas: ["Realización Integral Audiovisual 2"] },
   { nombre: "Estética", correlativas: ["Arte y Sociedad"] },
   { nombre: "Comunicación Audiovisual", correlativas: ["Taller de Lectura y Escritura profesional"] },
   { nombre: "Tecnología de Postproducción 1", correlativas: ["Tecnología de la Imagen", "Electrónica de las Comunicaciones"] },
   { nombre: "Imagen, Tipografía e Identidad Cultural", correlativas: ["Taller de Lectura y Escritura profesional"] }
 ],
-  "CUARTO AÑO": [
+"CUARTO AÑO": [
   { nombre: "Semiótica", correlativas: ["Taller de Lectura y Escritura profesional"] },
   { nombre: "Inglés II", correlativas: ["Inglés I"] },
   { nombre: "Metodología de Investigación", correlativas: ["Taller de Lectura y Escritura profesional"] },
@@ -51,13 +51,14 @@ const materiasPorAnio = {
   { nombre: "Tecnología de Postproducción 3", correlativas: ["Tecnología de Postproducción 2"] },
   { nombre: "Optativa 1", correlativas: [] }
 ],
-  "QUINTO AÑO": [
+"QUINTO AÑO": [
   { nombre: "Taller de Trabajo Final Integrador Mención Postproducción de Imagen", correlativas: ["Géneros Estilos Audiovisuales", "Gestión de proyectos Audiovisuales", "Estética", "Imagen, Tipografía e Identidad Cultural", "Semiótica"] },
   { nombre: "Animación 3D 2", correlativas: ["Animación 3D 1", "Composición Digital"] },
   { nombre: "Taller de Postproducción de Imagen", correlativas: ["Animación 3D 1", "Composición Digital"] },
   { nombre: "Práctica Preprofesional Mención Postproducción de Imagen", correlativas: ["Realización Integral Audiovisual 2"] },
   { nombre: "Optativa 2", correlativas: [] },
   { nombre: "Optativa 3", correlativas: [] }
+ ]
 };
 
 let aprobadas = new Set();
